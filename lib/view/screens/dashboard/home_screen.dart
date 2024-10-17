ebf01bec84e05a53c7a30da69fa5065eb9c09a54
Bithichakma <bithichakma431@gmail.com>
2024-10-17 23:11:56 +0600

    search section

--- lib/view/screens/dashboard/home_screen.dart
+++ lib/view/screens/dashboard/home_screen.dart
@@ -14,31 +14,46 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            SizedBox(height: 10,
+            ),
             Row(
               children: [
+                const SizedBox(width: 10),
                 IconButton(onPressed: () {}, icon: Icon(Icons.qr_code_scanner)),
                   Expanded(
                     child: Container(
-                          color: Colors.blue,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                         color: Colors.white,
+                         border: Border.all(width: 1.5, color: Colors.blue ),
+                         borderRadius: BorderRadius.circular(10)
+                         ),
                           child: Row(
-                            children: [
+                            children:[
                               Icon(Icons.camera_alt),
                               SizedBox(
                                 width: 10,
                               ),
-                              Expanded(child: TextField()),
+                              Expanded(child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Searching",
+                                  border: InputBorder.none),
+                              )),
+                              IconButton(onPressed: (){}, icon: Icon(Icons.search)),
                               Container(
-                                color: Colors.blue,
+                                //color: Colors.blue,
                                 child: Text("Search"),
                               ),
                             ],
                           ),
                           ),
                   ),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.notifications_outlined)),
+                  IconButton(
+                    onPressed: () {}, icon: Icon(Icons.notifications_outlined)),
+                  const SizedBox(width: 10),
               ],
             ),
-            Center(child: Text("Home Screen")),
+            //Center(child: Text("Home Screen")),
           ],
         ),
       ),
