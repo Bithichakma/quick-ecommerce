50bacecc8357bc9dc056bee2e225725bc381e1a5
Bithichakma <bithichakma431@gmail.com>
2024-10-24 21:20:39 +0600

    carrosel slider

--- lib/view/screens/dashboard/home_screen.dart
+++ lib/view/screens/dashboard/home_screen.dart
@@ -1,9 +1,17 @@
 
 
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+List<String> bannerImageLinks = [
+ "https://img.freepik.com/free-psd/banner-template-online-shopping_23-2148559048.jpg?semt=ais_hybrid",
+ "https://img.freepik.com/free-psd/e-commerce-concept-facebook-template_23-2151243790.jpg",
+ "https://img.freepik.com/free-psd/online-shopping-banner-template-style_23-2148545562.jpg?semt=ais_hybrid",
+ "https://img.freepik.com/free-psd/online-shopping-concept-banner-template_23-2148559464.jpg?semt=ais_hybrid",
+];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -36,14 +44,10 @@ class HomeScreen extends StatelessWidget {
                               ),
                               Expanded(child: TextField(
                                 decoration: InputDecoration(
-                                  hintText: "Searching",
+                                  hintText: "Search",
                                   border: InputBorder.none),
                               )),
                               IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-                              Container(
-                                //color: Colors.blue,
-                                child: Text("Search"),
-                              ),
                             ],
                           ),
                           ),
@@ -53,7 +57,26 @@ class HomeScreen extends StatelessWidget {
                   const SizedBox(width: 10),
               ],
             ),
-            //Center(child: Text("Home Screen")),
+            SizedBox(
+              height: 10,
+            ),
+            CarouselSlider(items: [
+              ...bannerImageLinks.map((value) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                    value,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                );
+
+              }),
+            ], options: CarouselOptions(
+              enlargeCenterPage: false,
+              aspectRatio: 16 / 9,
+              viewportFraction: 0.8,
+            ))
           ],
         ),
       ),
