779a83fe9f8d8cf94712415fcd220ddd752d32d6
Bithichakma <bithichakma431@gmail.com>
2024-11-17 22:44:25 +0600

    New Deals card

--- lib/view/screens/bottom_nav_bar/home/home_screen.dart
+++ lib/view/screens/bottom_nav_bar/home/home_screen.dart
@@ -50,62 +50,82 @@ class DealsSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10,right: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-              HomeTitle(title: "New Deals Everyday"),
-            Container(
-              padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          HomeTitle(title: "New Deals Everyday"),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
             decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(10)
-              ),
+                color: Colors.grey, borderRadius: BorderRadius.circular(10)),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text(
-                  "7% OFF",
-                  style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),
-                  ),
-                Text(
-                  "Min. spend ৳ 3990 ",
-                  style: TextStyle(fontWeight: FontWeight.w300,color: Colors.blue),
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "7% OFF",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    Text(
+                      "Min. spend ৳ 3990 ",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Quick Voucher",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      width: 120,
+                      child: Text(
+                        "10-30-2024 to 11-06-2024",
+                        maxLines: 1,
+                        style: TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+               GestureDetector(onTap: () {}, 
+                child: Text("Collect all"),
+                )
               ],
-              ),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text(
-                  "Quick Voucher",
-                  style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),
-                  ),
-                Text(
-                  "10-30-2024 to 11-06-2024",
-                  style: TextStyle(fontWeight: FontWeight.w300,color: Colors.blue),
-                  ),
-              ],
-              ),
-            ],),
             ),
-            SizedBox(height: 10,),
-              HomeTitle(title: "Best Deals"),
-            Container(color: 
-            Colors.deepPurple,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          HomeTitle(title: "Best Deals"),
+          Container(
+            color: Colors.deepPurple,
             height: 100,
             width: 200,
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
-
-
