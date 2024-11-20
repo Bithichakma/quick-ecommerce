c1613582a718787691774f90b556735876c4872d
Bithichakma <bithichakma431@gmail.com>
2024-11-20 23:23:57 +0600

    Flash card Product

--- lib/view/screens/bottom_nav_bar/home/home_screen.dart
+++ lib/view/screens/bottom_nav_bar/home/home_screen.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:quick_ecommerce/view/screens/bottom_nav_bar/home/componets/deals_section.dart';
 import 'package:quick_ecommerce/view/screens/bottom_nav_bar/home/componets/home%20title.dart';
@@ -46,27 +47,82 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class FlashSaleSection extends StatelessWidget {
-  const FlashSaleSection({super.key});
+  const FlashSaleSection(
+      {super.key,
+      required this.ImageLink,
+      required this.mainPrice,
+      required this.oldPrice});
+  final String ImageLink;
+  final String mainPrice;
+  final String oldPrice;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10,right: 10),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              HomeTitle(title: "Flash Sale"),
-              TextButton(onPressed: () {}, child: Text("shop more"))
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-        ],
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(6),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                HomeTitle(title: "Flash Sale"),
+                TextButton(onPressed: () {}, child: Text("shop more"))
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FlashSalePtoductCard(),
+                FlashSalePtoductCard(),
+                FlashSalePtoductCard(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
+class FlashSalePtoductCard extends StatelessWidget {
+  const FlashSalePtoductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.all(6),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+                "https://www.lightxeditor.com/photo-editing/ai-product-photo-editor/"),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "$mainPrice tk",
+            style: TextStyle(color: Colors.orange),
+          ),
+          Text(
+            "$oldPrice tk",
+            style: TextStyle(
+                decoration: TextDecoration.lineThrough, color: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+}
